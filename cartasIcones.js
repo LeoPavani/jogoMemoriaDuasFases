113a0fa12b725a7f8dc5d67a3ed7aa50ea05224c
LeoPavani <leonardopavani.lp@hotmail.com>
2020-12-14 19:27:24 -0300

    duas fases estruturadas

---
+++ cartasIcones.js
@@ -0,0 +1,58 @@
+const carta1 = `<div class="carta primeiroPar verso" id="1"></div>`;
+const carta2 = `<div class="carta primeiroPar verso" id="2"></div>`;
+const carta3 = `<div class="carta segundoPar verso" id="3"></div>`;
+const carta4 = `<div class="carta segundoPar verso" id="4"></div>`;
+const carta5 = `<div class="carta terceiroPar verso" id="5"></div>`;
+const carta6 = `<div class="carta terceiroPar verso" id="6"></div>`;
+const carta7 = `<div class="carta quartoPar verso" id="7"></div>`;
+const carta8 = `<div class="carta quartoPar verso" id="8"></div>`;
+const carta9 = `<div class="carta quintoPar verso" id="9"></div>`;
+const carta10 = `<div class="carta quintoPar verso" id="10"></div>`;
+const carta11 = `<div class="carta sextoPar verso" id="11"></div>`;
+const carta12 = `<div class="carta sextoPar verso" id="12"></div>`;
+const carta13 = `<div class="carta setimoPar verso" id="13"></div>`;
+const carta14 = `<div class="carta setimoPar verso" id="14"></div>`;
+const carta15 = `<div class="carta oitavoPar verso" id="15"></div>`;
+const carta16 = `<div class="carta oitavoPar verso" id="16"></div>`;
+const carta17 = `<div class="carta nonoPar verso" id="17"></div>`;
+const carta18 = `<div class="carta nonoPar verso" id="18"></div>`;
+
+const arrayDeCartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18 ];
+const arrayTeste = [carta1, carta2];
+const embaralhadoTeste = shuffle(arrayTeste);
+const embaralhadoOficial = shuffle(arrayDeCartas);
+const body = document.querySelector(".game");
+const teste = `${embaralhadoTeste[0]}${embaralhadoTeste[1]}`;
+const realOficial = `${embaralhadoOficial[0]}${embaralhadoOficial[1]}${embaralhadoOficial[2]}${embaralhadoOficial[3]}${embaralhadoOficial[4]}${embaralhadoOficial[5]}${embaralhadoOficial[6]}${embaralhadoOficial[7]}${embaralhadoOficial[8]}${embaralhadoOficial[9]}${embaralhadoOficial[10]}${embaralhadoOficial[11]}${embaralhadoOficial[12]}${embaralhadoOficial[13]}${embaralhadoOficial[14]}${embaralhadoOficial[15]}${embaralhadoOficial[16]}${embaralhadoOficial[17]}`;
+
+embaralhaCartas(body, embaralhadoTeste, embaralhadoOficial);
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  
+  return array;
+}
+
+function embaralhaCartas(){
+  body.innerHTML = 
+    `
+      <div class="jogo">
+        ${teste}
+      </div>
+    `
+  ;
+}
---
+++ jogo.js
@@ -0,0 +1,174 @@
+const elemento = document.querySelector(".jogo");
+const elemParaModal = document.querySelector("#modal");
+const telaNaoClicavel = document.querySelector("#tela");
+
+let elementosClicados = [];
+let cartasClicadasPorRodada = 0;
+let primeiroId = 0;
+let segundoId = 0;
+let pontos = 0;
+const numeroDePares = defineNumeroDePares();
+const tempoAntesDaCartaErradaVirarDeVolta = 2000;
+const tempoParaAparecerMensagemVitoria = 800;
+const tempoNaoClicavel = 2000;
+const tempoParaVirarACarta = 200;
+const escalaInteira = "scale(1)";
+const escalaEnquantoCartaVira = "scale(0.1, 1)";
+
+
+elemento.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const clicado = event.target;
+  segundoId = clicado.id
+  const virado = clicado.className;
+  
+  verificaSePodeMostrarCarta(virado, clicado)
+    
+  primeiroId = clicado.id;
+});
+
+
+function animaACartaVirando(carta){
+  carta.animate({"transform" : escalaEnquantoCartaVira}, tempoParaVirarACarta);
+    setTimeout(() => {
+      trocaClasseVerso(carta);
+      carta.style.transform = escalaEnquantoCartaVira;
+      carta.animate({"transform" : escalaInteira}, tempoParaVirarACarta);
+      setTimeout(() => {
+        carta.style.transform = escalaInteira;
+
+      }, tempoParaVirarACarta);
+  }, (tempoParaVirarACarta - 30));
+}
+
+function colocaModalVitoria(elemParaModal){
+  elemParaModal.classList.remove("escondeModal");
+}
+
+function colocaNoVetorDeVerificacao(clicado){
+  //Para fazer a verificação(comparação), coloquei em um vetor...
+  const classeElementoClicado = clicado.className;
+  elementosClicados.push(classeElementoClicado);
+} 
+
+function defineNumeroDePares(){
+  const numeroDeCartas = elemento.childNodes.length - 2;
+  const numeroDosPares = numeroDeCartas / 2;
+  
+  return numeroDosPares;
+}
+
+function desviraCarta(){
+  const primeiro = document.getElementById(primeiroId);
+  const segundo = document.getElementById(segundoId);
+  
+  setTimeout(() => {
+    animaACartaVirando(primeiro);
+    tocarSomDesvirando();
+    animaACartaVirando(segundo);
+  }, tempoAntesDaCartaErradaVirarDeVolta);
+
+}
+
+function fazValidacoes(cartasClicadasPorRodada, clicado){
+  if (verificaSeUmaDuplaJaFoiClicada(cartasClicadasPorRodada)){
+    verificaDupla(elementosClicados, clicado);
+    verificaVitoria(numeroDePares);
+  }
+}
+
+function invalidaCliques(){
+  telaNaoClicavel.classList.add("naoClicavel");
+}
+
+function seCartaEstaViradaParaBaixo(virado){
+  if (virado.indexOf('verso') !== -1){
+    return true;
+  }else{
+    return false;
+  }
+} 
+
+function tocarSomDeAcerto(){
+  var audioAcertou = new Audio();
+  audioAcertou.src = "./sons/success.wav";
+  audioAcertou.play();
+}
+
+function tocarSomDesvirando(){
+  var audio2 = new Audio();
+  audio2.src = "./sons/desviraCartaSom.wav";
+  audio2.play();
+}
+
+function tocarSomVirando(){
+  var audio1 = new Audio();
+  audio1.src = "./sons/viraCartaSom.wav";
+  audio1.play();
+}
+
+function tocarSomVitoria(){
+  var audioVitoria = new Audio();
+  audioVitoria.src = "./sons/yeah.wav";
+  audioVitoria.play();
+}
+
+function trocaClasseVerso(alvo){
+  alvo.classList.toggle("verso");
+}
+
+function validaCliques(){
+  telaNaoClicavel.classList.remove("naoClicavel");
+}
+
+function verificaDupla(){
+  //verifica as classes (se for igual, a imagem também será)...
+  if(elementosClicados[0] === elementosClicados[1]){
+    setTimeout(() => {
+      tocarSomDeAcerto();
+    }, tempoParaVirarACarta * 3);
+    pontos ++;
+  }else{
+    invalidaCliques();
+    setTimeout(() => {
+      validaCliques();
+    }, tempoNaoClicavel);
+    desviraCarta();
+  }
+  elementosClicados = [];
+  cartasClicadasPorRodada -= 2;
+}
+
+function verificaSePodeMostrarCarta(virado, clicado){
+  if (seCartaEstaViradaParaBaixo(virado) && clicado.id){
+    viraCarta(clicado);
+    colocaNoVetorDeVerificacao(clicado);
+    fazValidacoes(cartasClicadasPorRodada, clicado);
+    cartasClicadasPorRodada ++;
+  }
+}
+
+function verificaSeUmaDuplaJaFoiClicada(cartasClicadasPorRodada){
+  if (cartasClicadasPorRodada > 0){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+function verificaVitoria(numeroDePares){
+  if(pontos == numeroDePares){
+    setTimeout(() => {
+      tocarSomVitoria();
+      colocaModalVitoria(elemParaModal);
+    }, tempoParaAparecerMensagemVitoria);
+  }else{
+    console.log("Ainda não...")
+  } 
+}
+
+function viraCarta(clicado){
+  animaACartaVirando(clicado);
+  tocarSomVirando();
+}
