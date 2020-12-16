c44f9c56b46ec20bf0fcff4c4027c4019052cba3
LeoPavani <leonardopavani.lp@hotmail.com>
2020-12-16 11:09:28 -0300

    arrumei os sons que não estavam saindo

--- js/jogo.js
+++ js/jogo.js
@@ -92,25 +92,25 @@ function seCartaEstaViradaParaBaixo(virado){
 
 function tocarSomDeAcerto(){
   var audioAcertou = new Audio();
-  audioAcertou.src = "./sons/success.wav";
+  audioAcertou.src = "../sons/success.wav";
   audioAcertou.play();
 }
 
 function tocarSomDesvirando(){
   var audio2 = new Audio();
-  audio2.src = "./sons/desviraCartaSom.wav";
+  audio2.src = "../sons/desviraCartaSom.wav";
   audio2.play();
 }
 
 function tocarSomVirando(){
   var audio1 = new Audio();
-  audio1.src = "./sons/viraCartaSom.wav";
+  audio1.src = "../sons/viraCartaSom.wav";
   audio1.play();
 }
 
 function tocarSomVitoria(){
   var audioVitoria = new Audio();
-  audioVitoria.src = "./sons/yeah.wav";
+  audioVitoria.src = "../sons/yeah.wav";
   audioVitoria.play();
 }
 
